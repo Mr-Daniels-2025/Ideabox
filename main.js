53d8a2b4b8dc708ce8481852fbae90ee7695c1ae
Hunter Monroe <huntis@outlook.com>
2022-07-27 15:32:18 -0600

    Add querySelectors and global variable

---
+++ main.js
@@ -0,0 +1,11 @@
+// form inputs
+var formTitle = document.querySelector(".title-input")
+var formBody = document.querySelector(".body-input")
+var searchBar = document.querySelector(".search-input")
+
+// buttons
+var showStarredButton = document.querySelector(".show-ideas")
+var saveButton = document.querySelector(".show-ideas")
+var searchButton = document.querySelector(".search-img")
+
+var userIdeas = []
