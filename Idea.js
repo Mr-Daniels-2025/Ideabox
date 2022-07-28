4e2d15eeb07c6ea04aa3ddf2f2b2de352016cc7a
Beth Wilson <bethwilson777@gmail.com>
2022-07-27 18:00:53 -0600

    Add inputValues function and instantiated a new class

--- Idea.js
+++ Idea.js
@@ -1,9 +1,14 @@
 class Idea{
   constructor(title, body){
-    this.id = date.now()
+    this.id = Date.now()
     this.title = title
     this.body = body
     this.isStarred = false
   }
 
+}
+
+function inputValues(){
+  newIdea = new Idea(formTitle.value, formBody.value)
+  userIdeas.push(newIdea)
 }
