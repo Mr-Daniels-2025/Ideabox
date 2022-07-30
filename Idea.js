f2088b200eb14b6e00542cbb053c15fb2b26f362
Hunter Monroe <huntis@outlook.com>
2022-07-30 15:11:32 -0600

    Update updateidea function

--- Idea.js
+++ Idea.js
@@ -1,20 +1,19 @@
-class Idea{
-  constructor(title, body){
+class Idea {
+  constructor(title, body) {
     this.id = Date.now()
     this.title = title
     this.body = body
     this.isStarred = false
   }
-  updateIdea(){
-  // use the updated svg logic to change the boolean value
+  updateIdea() {
+    // use the updated svg logic to change the boolean value
     if (!this.isStarred) {
       this.isStarred = true
-  } else {
-    this.isStarred = false
+    } else {
+      this.isStarred = false
+    }
   }
 }
-}
-
 // function inputValues(){
 //   var newIdea = new Idea(formTitle.value, formBody.value)
 //   userIdeas.push(newIdea)
